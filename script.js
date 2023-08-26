4266229fc5bab5d70075f101384ce265db7c6ebe
[PUZ]Cklebepand <116970894+nagibator3000-228@users.noreply.github.com>
2023-08-26 20:51:51 +0200

    added all stats color change

--- script.js
+++ script.js
@@ -41,7 +41,53 @@ $(document).ready(() => {
 });
 
 function check_stats() {
-   if (parseInt(document.getElementById("health").innerText) < 50) {
+   if (parseInt(document.getElementById("health").innerText) < 51) {
       document.getElementById("health").classList.add("text-warning");
+   } else if (parseInt(document.getElementById("health").innerText) < 26) {
+      document.getElementById("health").classList.add("text-danger");
+   } else if (parseInt(document.getElementById("health").innerText) < 0) {
+      document.getElementById("health").innerText = 0;
+   } else {
+      document.getElementById("health").classList.add("text-succes");
+   }
+
+   if (parseInt(document.getElementById("rad").innerText) > 49) {
+      document.getElementById("rad").classList.add("text-warning");
+   } else if (parseInt(document.getElementById("rad").innerText) > 99) {
+      document.getElementById("rad").classList.add("text-danger");
+   } else if (parseInt(document.getElementById("rad").innerText) < 20) {
+      document.getElementById("rad").innerText = 20;
+   } else {
+      document.getElementById("rad").classList.add("text-succes");
+   }
+
+   if (parseInt(document.getElementById("bio").innerText) > 29){
+      document.getElementById("bio").classList.add("text-warning");
+   } else if (parseInt(document.getElementById("bio").innerText) > 79) {
+      document.getElementById("bio").classList.add("text-danger");
+   } else if (parseInt(document.getElementById("bio").innerText) < 0) {
+      document.getElementById("bio").innerText = 0;
+   } else {
+      document.getElementById("bio").classList.add("text-succes");
+   }
+
+   if (parseInt(document.getElementById("psy").innerText) > 99) {
+      document.getElementById("psy").classList.add("text-warning");
+   } else if (parseInt(document.getElementById("psy").innerText) > 199) {
+      document.getElementById("psy").classList.add("text-danger");
+   } else if (parseInt(document.getElementById("psy").innerText) < 0) {
+      document.getElementById("psy").innerText = 0;
+   } else {
+      document.getElementById("psy").classList.add("text-succes");
+   }
+   
+   if (parseInt(document.getElementById("temp").innerText) > 49) {
+      document.getElementById("temp").classList.add("text-warning");
+   } else if (parseInt(document.getElementById("temp").innerText) > 129) {
+      document.getElementById("temp").classList.add("text-danger");
+   } else if (parseInt(document.getElementById("temp").innerText) < 23) {
+      document.getElementById("temp").innerText = 23;
+   } else {
+      document.getElementById("temp").classList.add("text-succes");
    }
 }
