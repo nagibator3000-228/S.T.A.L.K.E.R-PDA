d91ca72e91bb8c120375333c03843eb037c89343
[PUZ]Cklebepand <116970894+nagibator3000-228@users.noreply.github.com>
2023-08-26 20:01:11 +0200

    added 404 error

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.static(__dirname));
+app.use((req, res, next) => {
+   res.status(404).send("Слышь, брат, страница не найдена! Чё ты тут забыл?");
+});
 
 var sockets = [];
 var count_of_sockets = 0;
@@ -70,11 +73,11 @@ io.on("connection", (socket) => {
    });
 
    socket.on("admin-send", (data) => {
-      const parsed_data = JSON.parse(data);
       if (parsed_data.task !== '') {
+         const parsed_data = JSON.parse(data);
          io.in(parsed_data.group).emit('get-task', JSON.stringify(parsed_data));
       } else {
-         io.in(parsed_data.group).emit('clear', JSON.stringify(parsed_data));
+         io.in(parsed_data.group).emit('clear');
       }
    });
 
